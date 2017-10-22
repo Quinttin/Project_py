82f22f7ce2a146fc3b89c63f4b6f22d0e373a309
Quinttin <32525393+Quinttin@users.noreply.github.com>
2017-10-22 17:35:47 -0400

    change x=z
    change x =z

---
+++ AraayProject.py
@@ -0,0 +1,19 @@
+import numpy as np
+A = np.array([0,1,2,3,4,5,6,7,8,9,10])
+B = np.array([0,1,2,3,4,5,6,7,8,9,10])
+
+
+resultarray1=list(A)
+resultarray2=list(B)
+
+for x in resultarray1 ,resultarray2:
+    print(x)
+totaalist3 =  sum(A) + sum(B)
+leng = len(A) + len(B)
+average = float(totaalist3/leng)
+print(average)
+
+
+
+
+
