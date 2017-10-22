dfe1d42c381edf75d9cfbb3ee2601ced9c3caf8e
Quinttin <32525393+Quinttin@users.noreply.github.com>
2017-10-22 17:38:39 -0400

    change x=z
    just test it

---
+++ NewProject.py
@@ -0,0 +1,50 @@
+#list
+list1=[1,2,3,4]
+list2=[4,5,6,7]
+ziplist=zip(list1,list2)
+
+#for
+x=0
+for z in ziplist:
+    print(z)
+
+#while loop
+count= 0
+while count < 5:
+    print(count)
+    count = count+1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
